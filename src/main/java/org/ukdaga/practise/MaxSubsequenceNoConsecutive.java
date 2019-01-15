94fa6478689e9f7c96bfd8b32660f0c76b16c8b4
Ujjwal Daga <e076166@GH-5MJZFH2.corp.mastercard.org>
2019-01-15 16:00:42 +0530

    debugs

--- src/main/java/org/ukdaga/practise/MaxSubsequenceNoConsecutive.java
+++ src/main/java/org/ukdaga/practise/MaxSubsequenceNoConsecutive.java
@@ -11,14 +11,19 @@ public class MaxSubsequenceNoConsecutive
 		int excl_new; 
 		int i; 
 
+		System.out.println("|Current element : "+arr[0]);
 		for (i = 1; i < n; i++) 
 		{ 
 			/* current max excluding i */
 			excl_new = (incl > excl) ? incl : excl; 
+			System.out.print("|Current element : "+arr[i]+" |Old inclusive : "+incl+" |Old exclusive : "+excl);
 
 			/* current max including i */
 			incl = excl + arr[i]; 
+			System.out.print("|New inclusive : "+incl+" |New exclusive : "+excl_new);
 			excl = excl_new; 
+			
+			System.out.println();
 		} 
 
 		/* return max of incl and excl */
@@ -29,10 +34,17 @@ public class MaxSubsequenceNoConsecutive
 	public static void main(String[] args) 
 	{ 
 		MaxSubsequenceNoConsecutive sum = new MaxSubsequenceNoConsecutive(); 
-		int arr[] = new int[]{5, 5, 10, 100, 10, 5}; 
+		int arr[] = new int[]{200,90,80,90,80,10, 12, 10, 5}; 
 		System.out.println(sum.FindMaxSum(arr, arr.length)); 
 	} 
 } 
 
 // This code has been contributed by Mayank Jaiswal 
-
+//start , next
+/*200 , 90
+280 , 90
+280 , 180 
+360 , 180
+360 , 190
+372 , 200
+377*/
